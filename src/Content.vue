4805eaf6f559b9b36855379a984053acd989ea15
SampurnaC <sampurna.chapagainnn@gmail.com>
2017-12-01 12:26:21 +0545

    update dependencies and add the Inbox component

---
+++ src/Content.vue
@@ -0,0 +1,36 @@
+<template>
+  <aside class = "lg-side">
+    <div class = "inbox-head">
+      <h3>{{ currentView.title }}</h3>
+
+    </div>
+    <keep-alive>
+      <component :is = "currentView.tag"></component>
+    </keep-alive>
+  </aside>
+</template>
+
+<script>
+  import Inbox from './Inbox.vue';
+  export default {
+    data() {
+      return {
+        history: [
+          {
+            tag: 'app-inbox',
+            title: 'Inbox'
+          }
+        ]
+      };
+    },
+    computed: {
+      currentView() {
+        return this.history[0];
+      }
+    },
+    components: {
+      appInbox: Inbox
+    }
+
+  }
+</script>
---
+++ src/Sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+  <aside class = "sm-side">
+    <div class = "user-head">
+      <img src = "src/assets/images/profile.jpg">
+      <div class = "user-name">
+        <h5>Sampurna Chapagain</h5>
+        <span class = "email-address">info@codingexplained.com</span>
+      </div>
+    </div>
+
+    <ul class = "inbox-nav">
+      <li class = "active">
+        <a href = "#" @click.prevent = "navigate('app-inbox', 'Inbox')">
+          <i class = "fa fa-envelope-o"></i>Inbox <span class = "label label-danger pull-right">?</span>
+        </a>
+      </li>
+
+      <li>
+        <a href = "#" @click.prevent = "navigate('app-sent', 'Sent')">
+          <i class = "fa fa-envelope-o"></i>Sent <span class = "label label-danger pull-right">?</span>
+        </a>
+      </li>
+
+      <li>
+        <a href = "#" @click.prevent = "navigate('app-important', 'Important')">
+          <i class = "fa fa-bookmark-o"></i>Important<span class = "label label-danger pull-right">?</span>
+        </a>
+      </li>
+
+      <li>
+        <a href = "#" @click.prevent = "navigate('app-trash', 'Trash')">
+          <i class = "fa fa-trash-o"></i>Trash<span class = "label label-danger pull-right">?</span>
+        </a>
+      </li>
+
+    </ul>
+  </aside>
+</template>
+
+<script>
+  import { eventBus } from './main';
+  export default {
+
+  }
+</script>
